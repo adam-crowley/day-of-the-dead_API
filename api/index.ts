e19384f8ac502a7f973c2a003440ebc8018ad632
Adam Crowley <adamcrowley@gmail.com>
2024-06-12 16:13:04 +1200

    Resolve CORS issues

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { join } from 'node:path'
+import { join } from 'path'
 import cors from 'cors'
 
 import locations from './routes/locations'
@@ -7,33 +7,34 @@ import schedule from './routes/schedule'
 import events from './routes/events'
 
 const app = express()
-
 const port = process.env.PORT || 3000
 
-const options = [
+// Use cors middleware before defining any routes
+app.use(
   cors({
     origin: '*',
-    methods: '*',
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    allowedHeaders:
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
     credentials: true,
-  }),
-]
-
-app.listen(port, () => {
-  console.log('Listening on port', port)
-})
+  })
+)
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
 })
+
 app.use('/api/v1/locations', locations)
 app.use('/api/v1/schedule', schedule)
 app.use('/api/v1/events', events)
 app.use('/api/v1/*', (req, res) => res.sendStatus(404))
 
 app.use(express.static(join(__dirname, './public')))
-app.use(options)
+
+app.listen(port, () => {
+  console.log('Listening on port', port)
+})
 
 export default app
